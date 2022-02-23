5ea5d4d70c0177604fd6bb45da749c4e670f7cbd
jordi3d <jordi@cine3d.com>
2022-02-23 16:41:26 +0100

    VectorInt

--- src/pages/llista.js
+++ src/pages/llista.js
@@ -1,13 +1,16 @@
 import Comptador from "./Comptador";
 import Graella from "./Graella";
 import Acordio from "./Acordio";
+import VectorSencers from "./VectorSencers";
 import Contact from "./Contact";
+
 //import ToggleButton from "./ToggleButton";
 
 const llista = [
   { path: "comptador", titol: "Comptador", element: <Comptador /> },
   { path: "graella", titol: "Graella de fotos", element: <Graella /> },
   { path: "acordio", titol: "Menús desplegables", element: <Acordio /> },
+  { path: "vectorint", titol: "Vector de sencers", element: <VectorSencers /> },
   { path: "contact", titol: "Contacte", element: <Contact /> },
 ];
 export default llista;
