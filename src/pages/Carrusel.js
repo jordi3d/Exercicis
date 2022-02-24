85c9a19dfb5083088018ce39217f85849b4b94b4
jordi3d <jordi@cine3d.com>
2022-02-24 16:52:43 +0100

    millorant Carrusel

--- src/pages/Carrusel.js
+++ src/pages/Carrusel.js
@@ -3,5 +3,32 @@ import { useState } from "react";
 
 export default function Carrusel() {
   const [zooming, setZooming] = useState(true);
-  return <div id="carrusel">CARRUSEL</div>;
+  function amagamenus() {
+    let tmp1 = document.getElementById("anterior");
+    let tmp2 = document.getElementById("seguent");
+    tmp1.style.display = "none";
+    tmp2.style.display = "none";
+  }
+  function mostramenus() {
+    let tmp1 = document.getElementById("anterior");
+    let tmp2 = document.getElementById("seguent");
+    tmp1.style.display = "block";
+    tmp2.style.display = "block";
+    return true;
+  }
+  return (
+    <div id="carrusel">
+      <div id="anterior">&lt;</div>
+      <img
+        id="foto"
+        src="./img/imatge1.png"
+        alt="imatge #1"
+        onMouseOver={() => mostramenus()}
+        onMouseOut={() => amagamenus()}
+      />
+      <div id="seguent">&gt;</div>
+      <div id="separador"></div>
+      <div id="navegador">· · o · · </div>
+    </div>
+  );
 }
