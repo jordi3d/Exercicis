0af61b5d20cd871b2b0547c2d8025cb8856aa539
jordi3d <jordi@cine3d.com>
2022-02-24 02:54:23 +0100

    acabats tots els exercicis d'arrays

--- src/pages/ArrayObjectes.js
+++ src/pages/ArrayObjectes.js
@@ -1,5 +1,23 @@
 import { useState } from "react";
 
 export default function ArrayObjectes() {
-  return <div id="arrayobjectes">ARRAY OBJECTES</div>;
+  let array = ["u1", "u3"];
+
+  let objecte = {
+    u1: { name: "Pere" },
+    u2: { name: "Joan" },
+    u3: { name: "Maria" },
+  };
+  let resultat = [objecte[array[0]], objecte[array[1]]];
+
+  return (
+    <div id="arrayobjectes">
+      Array: {JSON.stringify(array)}
+      <br />
+      Objecte: {JSON.stringify(objecte)}
+      <br />
+      Resultat: {JSON.stringify(resultat[0])}
+      {JSON.stringify(resultat[1])}
+    </div>
+  );
 }
