455dcbb3e0760be2f0b2f5f223192e6b00a2e90b
jordi3d <jordi@cine3d.com>
2022-02-25 01:19:09 +0100

    corregint mal comportament carrusel

--- src/pages/Carrusel.js
+++ src/pages/Carrusel.js
@@ -1,34 +1,65 @@
 import "./Carrusel.css";
 import { useState } from "react";
 
+let fotos = [
+  "/img/imatge1.png",
+  "/img/imatge2.jpeg",
+  "/img/imatge3.jpeg",
+  "/img/imatge4.jpeg",
+  "/img/imatge5.jpeg",
+  "/img/imatge6.jpeg",
+].map((f) => process.env.PUBLIC_URL + f);
+
 export default function Carrusel() {
-  const [zooming, setZooming] = useState(true);
+  const [imatge, setImatge] = useState(0);
+
+  function fwd() {
+    console.log("CLICK FWD: %d", imatge);
+    let tmp1 = document.getElementById("carrusel");
+    if (imatge < fotos.length - 1) {
+      setImatge((n) => n + 1);
+    } else setImatge(0);
+    console.log("FWD %d", imatge);
+    tmp1.style.backgroundImage = 'url("' + fotos[imatge] + '")';
+  }
+  function rew() {
+    console.log("CLICK REW: %d", imatge);
+    let tmp1 = document.getElementById("carrusel");
+    if (imatge > 0) {
+      setImatge((n) => n - 1);
+    } else setImatge(fotos.length - 1);
+    tmp1.style.backgroundImage = 'url("' + fotos[imatge] + '")';
+  }
   function amagamenus() {
     let tmp1 = document.getElementById("anterior");
     let tmp2 = document.getElementById("seguent");
+    let tmp3 = document.getElementById("navegador");
     tmp1.style.display = "none";
     tmp2.style.display = "none";
+    tmp3.style.display = "none";
   }
   function mostramenus() {
     let tmp1 = document.getElementById("anterior");
     let tmp2 = document.getElementById("seguent");
+    let tmp3 = document.getElementById("navegador");
     tmp1.style.display = "block";
     tmp2.style.display = "block";
-    return true;
+    tmp3.style.display = "block";
   }
   return (
-    <div id="carrusel">
-      <div id="anterior">&lt;</div>
-      <img
-        id="foto"
-        src="./img/imatge1.png"
-        alt="imatge #1"
-        onMouseOver={() => mostramenus()}
-        onMouseOut={() => amagamenus()}
-      />
-      <div id="seguent">&gt;</div>
+    <div
+      id="carrusel"
+      onMouseOver={() => mostramenus()}
+      onMouseOut={() => amagamenus()}
+    >
+      <div id="anterior" onClick={() => rew()}>
+        &lt;
+      </div>
+      <div id="seguent" onClick={() => fwd()}>
+        &gt;
+      </div>
       <div id="separador"></div>
-      <div id="navegador">· · o · · </div>
+      <div id="navegador">· · o · · {imatge}</div>
     </div>
   );
 }
