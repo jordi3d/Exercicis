7b8d81181e38bc95adae10cf8fe8a7f2f6412b00
jordi3d <jordi@cine3d.com>
2022-02-27 16:36:59 +0100

    ajustats els divs de les hores

--- src/pages/RellotgeAnalogic.js
+++ src/pages/RellotgeAnalogic.js
@@ -8,44 +8,46 @@ export default function RellotgeAnalogic() {
     let sec_graus = d.getSeconds() * 6;
     return (
       <div className="mostra_watch">
-        <div id="dotze">12</div>
-        <div id="tres">3</div>
-        <div id="sis">6</div>
-        <div id="nou">9</div>
-        <svg viewBox="0 0 100 100" width="120" height="120">
-          <circle
-            cx="50"
-            cy="50"
-            r="40"
-            stroke="black"
-            stroke-width="1"
-            fill="gold"
-          ></circle>
-          <path
-            id="hora"
-            transform={"rotate(" + hora_graus + " 50 50)"}
-            d="M50 50v-20"
-            stroke="#000"
-            stroke-width="5"
-            stroke-linecap="round"
-          />
-          <path
-            id="minut"
-            transform={"rotate(" + min_graus + " 50 50)"}
-            d="M50 50v-35"
-            stroke="#000"
-            stroke-width="3"
-            stroke-linecap="round"
-          />
-          <path
-            id="segon"
-            transform={"rotate(" + sec_graus + " 50 50)"}
-            d="M50 50v-37"
-            stroke="#000"
-            stroke-width="1"
-            stroke-linecap="round"
-          />
-        </svg>
+        <div>
+          <div id="dotze">12</div>
+          <div id="tres">3</div>
+          <div id="sis">6</div>
+          <div id="nou">9</div>
+          <svg viewBox="0 0 100 100" width="120" height="120">
+            <circle
+              cx="50"
+              cy="50"
+              r="40"
+              stroke="black"
+              stroke-width="1"
+              fill="gold"
+            ></circle>
+            <path
+              id="hora"
+              transform={"rotate(" + hora_graus + " 50 50)"}
+              d="M50 50v-20"
+              stroke="#000"
+              stroke-width="5"
+              stroke-linecap="round"
+            />
+            <path
+              id="minut"
+              transform={"rotate(" + min_graus + " 50 50)"}
+              d="M50 50v-35"
+              stroke="#000"
+              stroke-width="3"
+              stroke-linecap="round"
+            />
+            <path
+              id="segon"
+              transform={"rotate(" + sec_graus + " 50 50)"}
+              d="M50 50v-37"
+              stroke="#000"
+              stroke-width="1"
+              stroke-linecap="round"
+            />
+          </svg>
+        </div>
       </div>
     );
   }
