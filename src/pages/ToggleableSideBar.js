538481853bbca24fd7bdd4c3bfacfaec07e2abf3
jordi3d <jordi@cine3d.com>
2022-02-27 16:50:44 +0100

    petit ajust a slider

--- src/pages/ToggleableSideBar.js
+++ src/pages/ToggleableSideBar.js
@@ -3,10 +3,17 @@ import { useState } from "react";
 
 export default function ToggleableSideBar() {
   const [clicked, setClicked] = useState(true);
+  const [entering, setEntered] = useState(true);
 
   return (
     <div id="toggleablesidebar">
-      <div id="boto" onClick={() => setClicked(!clicked)}>
+      <div
+        id="boto"
+        onClick={() => {
+          setEntered(false);
+          setClicked(!clicked);
+        }}
+      >
         <svg viewBox="9 0 10 8" width="115" height="8">
           <path
             d="M1 1h8M1 4h 8M1 7h8"
@@ -19,8 +26,10 @@ export default function ToggleableSideBar() {
       <div
         id="sidebar"
         style={{
-          animation: clicked
-            ? "slidein 0.6s linear 0s 1 normal forwards"
+          animation: entering
+            ? ""
+            : clicked
+            ? "slidein 0.3s linear 0s 1 normal forwards"
             : "slideout 0.6s linear 0.2s 1 normal forwards",
         }}
       ></div>
